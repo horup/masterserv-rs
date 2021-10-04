0d53fdf37568fd58c5465b4eb29c3d5237b64a6a
Søren Alsbjerg Hørup <sah@sah.dk>
2021-10-04 08:03:22 +0200

    worked on vscode tasks

--- dummy-game/src/lib.rs
+++ dummy-game/src/lib.rs
@@ -1,10 +1,11 @@
-//mod hosted;
-//pub use hosted::*;
+mod hosted;
+pub use hosted::*;
 
 use wasm_bindgen::prelude::*;
-
+use masterserv::log::info;
 
 #[wasm_bindgen]
-pub fn init() {
-    panic!("hi");
+pub fn start() {
+    wasm_logger::init(wasm_logger::Config::default());
+    info!("hi there!");
 }
