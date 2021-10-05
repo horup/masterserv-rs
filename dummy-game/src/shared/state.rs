a2204844c6c6b6b4f7439f0d97c2445e5e9a422e
Horup <sah@sah.dk>
2021-10-05 20:03:45 +0200

    worked on state

--- dummy-game/src/shared/state.rs
+++ dummy-game/src/shared/state.rs
@@ -1,3 +1,20 @@
+use glam::Vec2;
+
+pub struct Thing {
+    pub pos:Vec2,
+    pub vel:Vec2
+}
+
+impl Thing {
+    pub fn new() -> Self {
+        Self {
+            pos:[0.0, 0.0].into(),
+            vel:[0.0, 0.0].into()
+        }
+    }
+}
+
+
 pub struct GameState {
     pub x:f64
 }
